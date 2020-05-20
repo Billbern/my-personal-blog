47706153edf832cce34d912e5d3186391102e84f
Bernard Abaidoo <denis.abaidoo@gmail.com>
2020-05-20 23:48:31 +0000

    replaced load_json with sqlalchemy and sqlite

--- app.py
+++ app.py
@@ -1,85 +1,75 @@
 import os
-import json
 from time import strptime
-from flask_paginate import Pagination, get_page_args
-from flask import Flask, render_template, redirect, url_for, request
+from sqlalchemy import func
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
 
 
 blog = Flask(__name__)
+blog.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+blog.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(blog)
+
+from models import *
+
 
 # template for converting route string
 @blog.template_filter("convert_string")
 def convert_string(string):
     return string.lower().replace(' ', '-')
 
+
 # template for converting Month to number
 @blog.template_filter("format_datetime")
 def format_datetime(dto):
     return strptime(dto, '%B').tm_mon
 
-# load data from json file
-def load_data():
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-    return data
+def sidebar_data():
+    recent = Post.query.order_by(Post.posted_on.desc()).limit(5).all()
+    tags = Tag.query.all()
 
-# load requested paginated data
-def get_posts(data, offset=0, per_page=10):
-    return data[offset: offset + per_page]
+    return recent, tags
 
 
 # route for the index page
 @blog.route('/')
 @blog.route('/posts')
 def index():
-    # call the load_data function
-    data = load_data()["posts"]
-    
-    # get and set page parameters for pagination
-    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-    pagination_users = get_posts(data, offset=offset, per_page=per_page)
-    pagination = Pagination(page=page, per_page=per_page, total=len(data), css_framework='bootstrap4')
-    # return render_template('index.html', users=pagination_users, page=page, per_page=per_page, pagination=pagination )
-    
-    return render_template('index.html', title="Welcome to askAma", data=pagination_users,
-                                                                    pagination= pagination,
-                                                                    posts=data)
+    page = request.args.get('page', 1, type=int)
+    data = Post.query.paginate(page=page, per_page=10)
+    recent , tags = sidebar_data()
+    return render_template('index.html', title="Welcome to askAma", data=data.items, posts=recent, sidedata=tags)
+
 
 # route for individual posts
 @blog.route('/posts/<int:year>/<int:month>/<string:post_title>', methods=['GET', 'POST'])
 def post(year, month, post_title):
-    
-    # call load_data for only posts
-    data = load_data()["posts"]
-    
-    # check if requested page exists then render it
-    for item in data:
-        if item['postTitle'].lower().replace(' ', '-') == post_title:
-            return render_template('post.html', info=item, title=item['postTitle'])
+    return render_template('post.html')
 
 
+# route for tags
 @blog.route('/category/<tag>')
 def category(tag):
-    data = load_data()['posts']
-    newdata = []
-    for post in data:
-        if tag in post['tags']:
-            newdata.append(post)
-    return render_template('tag.html', caption=f'Posts under {tag}', data=newdata, posts=data)
+    data = Tag.query.filter_by(name=tag).all()[0].posts
+    sidedata = Tag.query.all()
+    posts = Post.query.all()
+    return render_template('tag.html', title=f'Tag - {tag}', data=data, posts=posts, sidedata=sidedata)
 
+
+# route for authors
 @blog.route('/author/<username>')
 def author(username):
-    data =load_data()['posts']
-    newdata = []
-    for post in data:
-        if username == post['author']:
-            newdata.append(post)
-    return render_template('author.html', caption=f'Posts by {username}', data=newdata, posts=data)
+    data = User.query.filter_by(username=username).all()[0].post
+    sidedata = Tag.query.all()
+    posts = Post.query.all()
+    return render_template('author.html', title=f'Author - {username}', data=data, sidedata=sidedata, posts=posts)
+
 
 # login route
 @blog.route('/login', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html", title="Signin to askAma")
+
 
 # signup route
 @blog.route('/signup')
---
+++ models.py
@@ -0,0 +1,69 @@
+from app import db
+from datetime import datetime
+
+non_url_safe = ['"', '\'', '#', '$', '%', '&', '+', ',', '/', ':', ';', '=',
+                    '?', '@', '[', '\\', ']', '^', '`', '{', '|', '}', '~']
+
+translate_table = {ord(char): u'' for char in non_url_safe}
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    pic = db.Column(db.String, nullable=False, default='/static/img/default-img.png')
+    password = db.Column(db.String(60), nullable=False)
+    post = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.pic}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(140), nullable=False)
+    slug = db.Column(db.String(140), nullable=False)
+    posted_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    summary = db.Column(db.Text, nullable=True)
+    content = db.Column(db.Text, nullable=False)
+    comment = db.relationship('Comment', backref='post', lazy='dynamic')
+    tags = db.relationship('Tag', secondary='tagging')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    @staticmethod
+    def _slugify(state, value, previous, initiator):
+        text = value.translate(translate_table)
+        state.slug = u'-'.join(text.lower().split())
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.posted_on}')"
+
+
+db.event.listen(Post.title, 'set', Post._slugify, retval=False)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner = db.Column(db.String(20), unique=True, nullable=False)
+    posted_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Comment('{self.owner}', '{self.content}', '{self.posted_on}')"
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    posts = db.relationship('Post', secondary='tagging')
+
+    def __repr__(self):
+        return f"Tag('{self.name}')"
+
+
+class Tagging(db.Model):
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+    post = db.relationship('Post', backref=db.backref('tagging', cascade="all, delete-orphan"))
+    tag = db.relationship('Tag', backref=db.backref('tagging', cascade="all, delete-orphan"))
