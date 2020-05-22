7e063d61aa8b80fe6b0cb40dcd412b1d7ab4512c
Bernard Abaidoo <denis.abaidoo@gmail.com>
2020-05-22 07:25:18 +0000

    added pagination and finished post

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from time import strptime
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request
 
@@ -12,19 +12,13 @@ db = SQLAlchemy(blog)
 from models import *
 
 
-# template for converting route string
-@blog.template_filter("convert_string")
-def convert_string(string):
-    return string.lower().replace(' ', '-')
-
-
 # template for converting Month to number
 @blog.template_filter("format_datetime")
 def format_datetime(dto):
-    return strptime(dto, '%B').tm_mon
+    return dto.strftime('%B %d, %Y')
 
 
-#sidebar data loader
+# sidebar data loader
 def sidebar_data():
     recent = Post.query.order_by(Post.posted_on.desc()).limit(5).all()
     tags = Tag.query.all()
@@ -37,29 +31,31 @@ def sidebar_data():
 @blog.route('/posts')
 def index():
     page = request.args.get('page', 1, type=int)
-    data = Post.query.paginate(page=page, per_page=10)
+    data = Post.query.order_by(Post.posted_on.desc()).paginate(page=page, per_page=6)
     recent , tags = sidebar_data()
-    return render_template('index.html', title="Welcome to askAma", data=data.items, posts=recent, sidedata=tags)
+    return render_template('index.html', title="Welcome to askAma", data=data, posts=recent, sidedata=tags)
 
 
 # route for individual posts
-@blog.route('/posts/<int:year>/<int:month>/<string:post_title>', methods=['GET', 'POST'])
-def post(year, month, post_title):
-    return render_template('post.html')
+@blog.route('/posts/<int:year>/<int:month>/<string:slug>', methods=['GET', 'POST'])
+def post(year, month, slug):
+    data = Post.query.filter_by(slug=slug).first()
+    return render_template('post.html', info=data)
 
 
 # route for tags
 @blog.route('/category/<tag>')
 def category(tag):
-    data = Tag.query.filter_by(name=tag).all()[0].posts
-    recent , tags = sidebar_data()
+    # page = request.args.get('page', 1, type=int)
+    data = Tag.query.filter_by(name=tag).first().posts
+    recent, tags = sidebar_data()
     return render_template('tag.html', title=f'Tag - {tag}', data=data, posts=recent, sidedata=tags)
 
 
 # route for authors
 @blog.route('/author/<username>')
 def author(username):
-    data = User.query.filter_by(username=username).all()[0].post
+    data = User.query.filter_by(username=username).first().post
     recent , tags = sidebar_data()
     return render_template('author.html', title=f'Author - {username}', data=data, sidedata=tags, posts=recent)
 
