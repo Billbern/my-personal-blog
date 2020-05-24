579bda0b003aae8723d6244be1df2d57da123afd
Bernard Abaidoo <denis.abaidoo@gmail.com>
2020-05-24 20:23:40 +0000

    added migrations

--- personalblog/__init__.py
+++ personalblog/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from personalblog.config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -6,7 +7,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 blog = Flask(__name__)
 blog.config.from_object(Config)
+
 db = SQLAlchemy(blog)
+migrate = Migrate(blog, db)
+
 loginmanager = LoginManager(blog)
 loginmanager.login_view = 'signin'
 loginmanager.login_message_category = 'info'
